21c3810f332387582570d7345d6c3be8f583c8b8
MYCOMPUTER\tokir <toki.ryuka.senriyama@gmail.com>
2019-07-27 21:59:37 +0900

    delete prev library

--- kirara/config.hpp
+++
@@ -1,9 +0,0 @@
-/*=============================================================================
-  Copyright (c) 2019 yu ishiyama
-=============================================================================*/
-#ifndef KIRARA_CONFIG_HPP
-#define KIRARA_CONFIG_HPP
-
-#include <kirara/config/namespace.hpp>
-
-#endif // KIRARA_CONFIG_HPP
