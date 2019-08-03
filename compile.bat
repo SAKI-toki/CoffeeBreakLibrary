d6d2cf29569e3eaa69dafdc81342124489bb409f
MYCOMPUTER\tokir <toki.ryuka.senriyama@gmail.com>
2019-08-03 21:46:12 +0900

    add gitignore

--- compile.bat
+++ compile.bat
@@ -5,18 +5,21 @@ for /r %%A in (src\*.cpp) do (
   set STR=!STR! %%A
 )
 
+rem command option
+set OPTION=-std=c++2a -Wall -Wextra --pedantic-error -O3 -I. --exec-charset=cp932 -o
+
 rem make bin directory
 IF NOT EXIST "bin" (
 mkdir bin
 )
 rem make exe
-g++ -std=c++2a -Wall -Wextra --pedantic-error -O3 -o bin\app.exe %STR% -I. --exec-charset=cp932
+g++ %OPTION% bin\app.exe  %STR%
 
 rem make asm directory
 IF NOT EXIST "asm" (
 mkdir asm
 )
 rem make assembly
-g++ -std=c++2a -Wall -Wextra --pedantic-error -O3 -o asm\asm.s -S %STR% -I. --exec-charset=cp932
+g++ %OPTION% asm\asm.s -S %STR%
 
 endlocal
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <coffee_break/operator.hpp>
+#include <coffee_break/pi.hpp>
+#include <coffee_break/size.hpp>
+#include <coffee_break/std.hpp>
+
+int main()
+{
+    return 0;
+}
